f4f4b7f542bdb2d95e6e85349c71622490920b97
dannyenfp <daniel.valmas@gmail.com>
2016-08-17 20:52:17 -0400

    removed all models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "haikus for the soul"
+    set :session_secret, "haikusforthesoul"
   end
 
 
